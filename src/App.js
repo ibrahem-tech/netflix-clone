53099935d6723e35e3cb8cb445647fafe110d1e4
Your Name <ryibrahem1210@gmail.com>
2020-08-21 15:32:01 -0400

    building the rows and syling them

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import './App.css';
-import Row from './components/Row'
+import React from "react";
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-   <h1>netflix clone</h1>
-   <Row title="NETFLIX ORIGINALS"/>
-   <Row title="Trending Now"/>
-   <Row title="Trending Now"/>
-
-
+    <div className="app">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Films" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
