58aa93add9eab6c2177878476deae75f86e6f89f
Your Name <ryibrahem1210@gmail.com>
2020-08-22 11:56:47 -0400

    building the navbar and functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Logo from "./signature.png";
+import "./Nav.css";
 
 function Nav() {
-  return <div></div>;
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src={Logo} alt="my-logo" />
+      <img
+        className="nav__avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
 }
 
 export default Nav;
